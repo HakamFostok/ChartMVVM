93515af430914fed571ba2fc17112fda1b135e4e
hfostok-plana <hfostok@planatechnologies.com>
2023-11-18 11:30:05 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Xceed.Maui.Toolkit;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new ViewModel();
+    }
+}
+
+public partial class ViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private IEnumerable<DataPoint> _points = [new(1, 10), new(2, 30)];
+}
